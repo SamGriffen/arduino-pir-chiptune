09801b3d7516e14826d6579b7faaadd96adefc87
Joshua-Benfell <31882036+Joshua-Benfell@users.noreply.github.com>
2017-10-27 22:29:08 +1300

    All Star and Tempo

--- platformio-project/src/speakers.cpp
+++ platformio-project/src/speakers.cpp
@@ -1,6 +1,6 @@
 // Include the arduino Libraries
 #include "Arduino.h"
-
+#include "math.h"
 // Define a constant for the PIR sensor
 const int PIR = 2;
 
@@ -12,26 +12,26 @@ const unsigned long del = 20000;
 // Define a variable for the last time the sensor triggered
 unsigned long lastTripped = 0;
 
-const long fn = 400; //Duration of 4 beats
-const long hn = 200; //Duration of 2 beats
-const long qn = 100; //Duartion of 1 beat
-const long en = 50;
-const long sn = 16;
+const long fn = 240; //Duration of 4 beats
+const long hn = 120; //Duration of 2 beats
+const long qn = 60; //Duartion of 1 beat
+const long en = 30;
+const long sn = 15;
 
 // Define the notes
-const long c[9]		= {16, 33, 65, 131, 262, 523, 1047, 2093, 4186};
-const long cs[9]	= {17, 35, 69, 139, 277, 554, 1109, 2217, 4435};
-const long d[9] 	= {18, 37, 73, 147, 294, 587, 1175, 2349, 4699};
-const long eb[9] 	= {19, 39, 78, 156, 311, 622, 1245, 2489, 4978};
-const long e[9]  	= {21, 41, 82, 165, 330, 659, 1319, 2637, 5274};
-const long f[9]  	= {22, 44, 87, 175, 349, 699, 1397, 2794, 5588};
-const long fs[9] 	= {23, 46, 93, 185, 370, 740, 1480, 2960, 5920};
-const long g[9]  	= {25, 49, 98, 196, 392, 784, 1568, 3136, 6272};
-const long gs[9] 	= {26, 52, 104, 208, 415, 831, 1661, 3322, 6645};
-const long a[9]  	= {28, 55, 110, 220, 440, 880, 1760, 3520, 7040};
-const long bb[9] 	= {29, 58, 117, 233, 466, 932, 1865, 3729, 7459};
-const long b[9]  	= {31, 62, 124, 247, 494, 988, 1976, 3951, 7902};
-const long r = 0;
+const long c[9]    = {16, 33, 65, 131, 262, 523, 1047, 2093, 4186};
+const long cs[9]  = {17, 35, 69, 139, 277, 554, 1109, 2217, 4435};
+const long d[9]   = {18, 37, 73, 147, 294, 587, 1175, 2349, 4699};
+const long eb[9]  = {19, 39, 78, 156, 311, 622, 1245, 2489, 4978};
+const long e[9]   = {21, 41, 82, 165, 330, 659, 1319, 2637, 5274};
+const long f[9]   = {22, 44, 87, 175, 349, 699, 1397, 2794, 5588};
+const long fs[9]  = {23, 46, 93, 185, 370, 740, 1480, 2960, 5920};
+const long g[9]   = {25, 49, 98, 196, 392, 784, 1568, 3136, 6272};
+const long gs[9]  = {26, 52, 104, 208, 415, 831, 1661, 3322, 6645};
+const long a[9]   = {28, 55, 110, 220, 440, 880, 1760, 3520, 7040};
+const long bb[9]  = {29, 58, 117, 233, 466, 932, 1865, 3729, 7459};
+const long b[9]   = {31, 62, 124, 247, 494, 988, 1976, 3951, 7902};
+const long r = 25000;
 
 /* WE ARE NUMBER ONE */
 // long song[][2] = {
@@ -58,70 +58,242 @@ const long r = 0;
 //   {cs[6],hn},
 //   {c[6],fn}
 //   };
-
+long bpm = 104;
 long song[][2] = {
-	{b[4], fn},
-	{r, qn},
-	{f[5], hn},
-	{d[5], hn},
-	{r, qn},
-	{d[5], fn},
-	{r, qn},
-	{c[5], hn},
-	{b[4], hn},
-	{r, qn},
-	{b[4], hn},
-	{e[5], hn}
+ {r     , hn},//1st bar start
+  {r     , qn},
+  {bb[4] , qn},//first bar end
+  {f[5]  , en},//2nd bar start
+  {d[5]  , en},
+  {d[5]  , qn},
+  {c[5]  , en},
+  {bb[4] , en},
+  {bb[4] , en},
+  {eb[5] , en},//2nd bar end
+  {eb[5] , en},//3rd bar start
+  {d[5]  , en},
+  {d[5]  , en},
+  {c[5]  , en},
+  {c[5]  , en},
+  {bb[4] , en},
+  {r     , en},
+  {bb[4] , en},//3rd bar end
+  {f[5]  , en},//4th bar start
+  {d[5]  , en},
+  {d[5]  , en},
+  {c[5]  , en},
+  {c[5]  , en},
+  {bb[4] , en},
+  {bb[4] , en},
+  {g[4]  , en},//4th bar end
+  {g[4]  , hn},//5th bar start
+  {r     , qn},
+  {bb[4] , en},
+  {bb[4] , en},//5th bar end
+  {f[5]  , en},//6th bar start
+  {d[5]  , en},
+  {d[5]  , en},
+  {c[5]  , en},
+  {c[5]  , en},
+  {bb[4] , en},
+  {bb[4] , en},
+  {eb[5] , en},//6th bar end
+  {eb[5] , en},//7th bar start
+  {d[5]  , en},
+  {d[5]  , en},
+  {c[5]  , en},
+  {c[5]  , en},
+  {bb[4] , en},
+  {bb[4] , qn},//7th bar end
+  {f[5]  , en},//8th bar start
+  {d[5]  , en},
+  {d[5]  , en},
+  {c[5]  , en},
+  {c[5]  , en},
+  {bb[4] , qn},
+  {bb[4] , qn},
+  {c[5]  , en},//8th bar end
+  {c[5]  , en+hn},//9th bar start
+  {r     , qn},
+  {bb[4] , en},
+  {g[4]  , en},//9th bar end
+  {bb[4] , en},//10th bar start
+  {bb[4] , en},
+  {bb[4] , sn},
+  {bb[4] , sn},
+  {bb[4] , sn},
+  {bb[4] , sn},
+  {bb[4] , en},
+  {bb[4] , en},
+  {bb[4] , en},
+  {bb[4] , en},//10th bar end
+  {bb[4] , en},//11th bar start
+  {bb[4] , sn},
+  {bb[4] , sn},
+  {bb[4] , en},
+  {bb[4] , sn},
+  {bb[4] , sn},
+  {bb[4] , sn},
+  {bb[4] , sn},
+  {bb[4] , en},
+  {d[5]  , en},
+  {bb[4] , en},//11th bar end
+  {d[5]  , sn},//12th bar start
+  {d[5]  , sn},
+  {f[5]  , en},
+  {eb[5] , en},
+  {d[5]  , sn},
+  {c[5]  , sn},
+  {c[5]  , en},
+  {c[5]  , en},
+  {c[5]  , en},
+  {b[5]  , en},//12th bar end
+  {d[5]  , en},//13th bar start
+  {bb[4] , en},
+  {bb[4] , en},
+  {f[4]  , sn},
+  {f[4]  , sn},
+  {bb[4] , en},
+  {bb[4] , en},
+  {g[4]  , en},
+  {f[4]  , en},//13th bar end
+  {d[5]  , sn},//14th bar start
+  {f[5]  , en},
+  {d[5]  , sn},
+  {g[5]  , en},
+  {d[5]  , sn},
+  {f[5]  , sn},
+  {f[5]  , sn},
+  {d[5]  , sn},
+  {g[5]  , en},
+  {d[5]  , en},
+  {f[5]  , en},//14th bar end
+  {eb[5] , en},//15th bar start
+  {d[5]  , en},
+  {c[5]  , en},
+  {bb[4] , en},
+  {c[5]  , en},
+  {bb[4] , en},
+  {r     , en},
+  {f[4]  , en},//15th bar end
+  {bb[4] , sn},//16th bar start
+  {bb[4] , sn},
+  {bb[4] , en},
+  {bb[4] , sn},
+  {bb[4] , sn},
+  {bb[4] , en},
+  {bb[4] , qn},
+  {r     , en},
+  {f[4]  , en},//16th bar end
+  {d[5]  , sn},//17th bar start
+  {bb[4] , sn},
+  {bb[4] , en},
+  {bb[4] , sn},
+  {bb[4] , sn},
+  {g[4]  , en},
+  {g[4]  , qn},
+  {r     , qn},//17th bar end
+  {d[5]  , en},//18th bar start
+  {bb[4] , qn},
+  {bb[4] , sn},
+  {g[4]  , sn},
+  {bb[4] , en},
+  {bb[4] , qn},
+  {bb[4] , sn},
+  {g[4]  , sn},//18th bar end
+  {bb[4] , en},//19th bar start
+  {bb[4] , qn},
+  {bb[4] , qn},
+  {d[5]  , qn},
+  {d[5]  , en},//19th bar end
+  {d[5]  , en},//20th bar start
+  {bb[4] , qn},
+  {bb[4] , sn},
+  {g[4]  , sn},
+  {bb[4] , en},
+  {bb[4] , qn},
+  {bb[4] , sn},
+  {g[4]  , sn},//20th bar end
+  {bb[4] , en},//21st bar start
+  {bb[4] , qn},
+  {bb[4] , qn},
+  {d[5]  , qn},
+  {d[5]  , en},//21st bar end
+  {d[5]  , qn},//22nd bar start
+  {f[5]  , qn},
+  {eb[5] , en},
+  {d[5]  , en},
+  {c[5]  , en},
+  {c[5]  , en},//22nd bar end
+  {c[5]  , en},//23rd bar start
+  {bb[4] , en+qn},
+  {bb[4] , en},
+  {bb[4] , en},
+  {c[5]  , en},
+  {bb[4] , en},//23rd bar end
+  {d[5]  , en},//24th bar start
+  {c[5]  , en+qn},
+  {c[5]  , en},
+  {bb[4] , qn},
+  {c[5]  , en},
+  {c[5]  , en},
+  {d[5]  , en},
+  {g[4]  , hn},
+  {r     , qn}
 };
+
+
 
 // Whether the speaker is currently playing
 bool isPlaying = 0;
 
 // Function to register a change in the PIR sensor (I.E. A person walks past)
 void registerChange(){
-	// Reset the timer
-	lastTripped = millis();
+  // Reset the timer
+  lastTripped = millis();
 
-	// Check whether to start playing
-	if(!isPlaying){
-		isPlaying = true;
-	}
+  // Check whether to start playing
+  if(!isPlaying){
+    isPlaying = true;
+  }
 }
 
 void setup() {
-	// put your setup code here, to run once:
-	Serial.begin(9600);
-	// Set the pinmode for the PIR pin
-	pinMode(PIR, INPUT_PULLUP);
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  // Set the pinmode for the PIR pin
+  pinMode(PIR, INPUT_PULLUP);
 
-	// Set the internal LED to output
-	pinMode(8, OUTPUT);
+  // Set the internal LED to output
+  pinMode(8, OUTPUT);
 
-	pinMode(speaker, OUTPUT);
+  pinMode(speaker, OUTPUT);
 
-	// Attach an interrupt to the PIR pin
-	attachInterrupt(digitalPinToInterrupt(PIR), registerChange, RISING);
+  // Attach an interrupt to the PIR pin
+  attachInterrupt(digitalPinToInterrupt(PIR), registerChange, RISING);
 }
 
 void loop() {
-	//	Check the timer. If the speaker is playing, and time is up, set the playing variable to false
-	if((millis() - lastTripped) >= del){
-		isPlaying = false;
-	}
+  //  Check the timer. If the speaker is playing, and time is up, set the playing variable to false
+  if((millis() - lastTripped) >= del){
+    isPlaying = false;
+  }
 
-	if(isPlaying){
-		for (int i = 0; i < (sizeof(song)/sizeof(song[0])); i++){
-			// If the song has been cancelled, break the loop
-			if(!isPlaying){
-				break;
-			}
-
-	  		tone(9, song[i][0]);
-	  		delay(song[i][1]);
-		}
-		noTone(9);
-	 	isPlaying = false;
-	}
+  if(isPlaying){
+    for (int i = 0; i < (sizeof(song)/sizeof(song[0])); i++){
+      // If the song has been cancelled, break the loop
+      if(!isPlaying){
+        break;
+      }
+        long timeDelay = floor((song[i][1]*1000)/bpm);
+        tone(9, song[i][0]);
+        delay(timeDelay);
+        tone(9, r);
+        delay(5);
+    }
+    noTone(9);
+    isPlaying = false;
+  }
 
 
 }
