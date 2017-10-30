a2a608a6e1575d6f26860d4a4a30eea2ba98700b
Joshua-Benfell <31882036+Joshua-Benfell@users.noreply.github.com>
2017-10-30 16:56:14 +1300

    Darude Sandstorm

--- platformio-project/src/speakers.cpp
+++ platformio-project/src/speakers.cpp
@@ -58,190 +58,311 @@ const long r = 25000;
 //   {cs[6],hn},
 //   {c[6],fn}
 //   };
-long bpm = 104;
-long song[][2] = {
- {r     , hn},//1st bar start
-  {r     , qn},
-  {bb[4] , qn},//first bar end
-  {f[5]  , en},//2nd bar start
-  {d[5]  , en},
-  {d[5]  , qn},
-  {c[5]  , en},
-  {bb[4] , en},
-  {bb[4] , en},
-  {eb[5] , en},//2nd bar end
-  {eb[5] , en},//3rd bar start
-  {d[5]  , en},
-  {d[5]  , en},
-  {c[5]  , en},
-  {c[5]  , en},
-  {bb[4] , en},
-  {r     , en},
-  {bb[4] , en},//3rd bar end
-  {f[5]  , en},//4th bar start
-  {d[5]  , en},
-  {d[5]  , en},
-  {c[5]  , en},
-  {c[5]  , en},
-  {bb[4] , en},
-  {bb[4] , en},
-  {g[4]  , en},//4th bar end
-  {g[4]  , hn},//5th bar start
-  {r     , qn},
-  {bb[4] , en},
-  {bb[4] , en},//5th bar end
-  {f[5]  , en},//6th bar start
-  {d[5]  , en},
-  {d[5]  , en},
-  {c[5]  , en},
-  {c[5]  , en},
-  {bb[4] , en},
-  {bb[4] , en},
-  {eb[5] , en},//6th bar end
-  {eb[5] , en},//7th bar start
-  {d[5]  , en},
-  {d[5]  , en},
-  {c[5]  , en},
-  {c[5]  , en},
-  {bb[4] , en},
-  {bb[4] , qn},//7th bar end
-  {f[5]  , en},//8th bar start
-  {d[5]  , en},
-  {d[5]  , en},
-  {c[5]  , en},
-  {c[5]  , en},
-  {bb[4] , qn},
-  {bb[4] , qn},
-  {c[5]  , en},//8th bar end
-  {c[5]  , en+hn},//9th bar start
-  {r     , qn},
-  {bb[4] , en},
-  {g[4]  , en},//9th bar end
-  {bb[4] , en},//10th bar start
-  {bb[4] , en},
-  {bb[4] , sn},
-  {bb[4] , sn},
-  {bb[4] , sn},
-  {bb[4] , sn},
-  {bb[4] , en},
-  {bb[4] , en},
-  {bb[4] , en},
-  {bb[4] , en},//10th bar end
-  {bb[4] , en},//11th bar start
-  {bb[4] , sn},
-  {bb[4] , sn},
-  {bb[4] , en},
-  {bb[4] , sn},
-  {bb[4] , sn},
-  {bb[4] , sn},
-  {bb[4] , sn},
-  {bb[4] , en},
-  {d[5]  , en},
-  {bb[4] , en},//11th bar end
-  {d[5]  , sn},//12th bar start
-  {d[5]  , sn},
-  {f[5]  , en},
-  {eb[5] , en},
-  {d[5]  , sn},
-  {c[5]  , sn},
-  {c[5]  , en},
-  {c[5]  , en},
-  {c[5]  , en},
-  {b[5]  , en},//12th bar end
-  {d[5]  , en},//13th bar start
-  {bb[4] , en},
-  {bb[4] , en},
-  {f[4]  , sn},
-  {f[4]  , sn},
-  {bb[4] , en},
-  {bb[4] , en},
-  {g[4]  , en},
-  {f[4]  , en},//13th bar end
-  {d[5]  , sn},//14th bar start
-  {f[5]  , en},
-  {d[5]  , sn},
-  {g[5]  , en},
-  {d[5]  , sn},
-  {f[5]  , sn},
-  {f[5]  , sn},
-  {d[5]  , sn},
-  {g[5]  , en},
-  {d[5]  , en},
-  {f[5]  , en},//14th bar end
-  {eb[5] , en},//15th bar start
-  {d[5]  , en},
-  {c[5]  , en},
-  {bb[4] , en},
-  {c[5]  , en},
-  {bb[4] , en},
-  {r     , en},
-  {f[4]  , en},//15th bar end
-  {bb[4] , sn},//16th bar start
-  {bb[4] , sn},
-  {bb[4] , en},
-  {bb[4] , sn},
-  {bb[4] , sn},
-  {bb[4] , en},
-  {bb[4] , qn},
-  {r     , en},
-  {f[4]  , en},//16th bar end
-  {d[5]  , sn},//17th bar start
-  {bb[4] , sn},
-  {bb[4] , en},
-  {bb[4] , sn},
-  {bb[4] , sn},
-  {g[4]  , en},
-  {g[4]  , qn},
-  {r     , qn},//17th bar end
-  {d[5]  , en},//18th bar start
-  {bb[4] , qn},
-  {bb[4] , sn},
-  {g[4]  , sn},
-  {bb[4] , en},
-  {bb[4] , qn},
-  {bb[4] , sn},
-  {g[4]  , sn},//18th bar end
-  {bb[4] , en},//19th bar start
-  {bb[4] , qn},
-  {bb[4] , qn},
-  {d[5]  , qn},
-  {d[5]  , en},//19th bar end
-  {d[5]  , en},//20th bar start
-  {bb[4] , qn},
-  {bb[4] , sn},
-  {g[4]  , sn},
-  {bb[4] , en},
-  {bb[4] , qn},
-  {bb[4] , sn},
-  {g[4]  , sn},//20th bar end
-  {bb[4] , en},//21st bar start
-  {bb[4] , qn},
-  {bb[4] , qn},
-  {d[5]  , qn},
-  {d[5]  , en},//21st bar end
-  {d[5]  , qn},//22nd bar start
-  {f[5]  , qn},
-  {eb[5] , en},
-  {d[5]  , en},
-  {c[5]  , en},
-  {c[5]  , en},//22nd bar end
-  {c[5]  , en},//23rd bar start
-  {bb[4] , en+qn},
-  {bb[4] , en},
-  {bb[4] , en},
-  {c[5]  , en},
-  {bb[4] , en},//23rd bar end
-  {d[5]  , en},//24th bar start
-  {c[5]  , en+qn},
-  {c[5]  , en},
-  {bb[4] , qn},
-  {c[5]  , en},
-  {c[5]  , en},
-  {d[5]  , en},
-  {g[4]  , hn},
-  {r     , qn}
+// All Star
+// long bpm = 104;
+// long song[][2] = {
+//  {r     , hn},//1st bar start
+//   {r     , qn},
+//   {bb[4] , qn},//first bar end
+//   {f[5]  , en},//2nd bar start
+//   {d[5]  , en},
+//   {d[5]  , qn},
+//   {c[5]  , en},
+//   {bb[4] , en},
+//   {bb[4] , en},
+//   {eb[5] , en},//2nd bar end
+//   {eb[5] , en},//3rd bar start
+//   {d[5]  , en},
+//   {d[5]  , en},
+//   {c[5]  , en},
+//   {c[5]  , en},
+//   {bb[4] , en},
+//   {r     , en},
+//   {bb[4] , en},//3rd bar end
+//   {f[5]  , en},//4th bar start
+//   {d[5]  , en},
+//   {d[5]  , en},
+//   {c[5]  , en},
+//   {c[5]  , en},
+//   {bb[4] , en},
+//   {bb[4] , en},
+//   {g[4]  , en},//4th bar end
+//   {g[4]  , hn},//5th bar start
+//   {r     , qn},
+//   {bb[4] , en},
+//   {bb[4] , en},//5th bar end
+//   {f[5]  , en},//6th bar start
+//   {d[5]  , en},
+//   {d[5]  , en},
+//   {c[5]  , en},
+//   {c[5]  , en},
+//   {bb[4] , en},
+//   {bb[4] , en},
+//   {eb[5] , en},//6th bar end
+//   {eb[5] , en},//7th bar start
+//   {d[5]  , en},
+//   {d[5]  , en},
+//   {c[5]  , en},
+//   {c[5]  , en},
+//   {bb[4] , en},
+//   {bb[4] , qn},//7th bar end
+//   {f[5]  , en},//8th bar start
+//   {d[5]  , en},
+//   {d[5]  , en},
+//   {c[5]  , en},
+//   {c[5]  , en},
+//   {bb[4] , qn},
+//   {bb[4] , qn},
+//   {c[5]  , en},//8th bar end
+//   {c[5]  , en+hn},//9th bar start
+//   {r     , qn},
+//   {bb[4] , en},
+//   {g[4]  , en},//9th bar end
+//   {bb[4] , en},//10th bar start
+//   {bb[4] , en},
+//   {bb[4] , sn},
+//   {bb[4] , sn},
+//   {bb[4] , sn},
+//   {bb[4] , sn},
+//   {bb[4] , en},
+//   {bb[4] , en},
+//   {bb[4] , en},
+//   {bb[4] , en},//10th bar end
+//   {bb[4] , en},//11th bar start
+//   {bb[4] , sn},
+//   {bb[4] , sn},
+//   {bb[4] , en},
+//   {bb[4] , sn},
+//   {bb[4] , sn},
+//   {bb[4] , sn},
+//   {bb[4] , sn},
+//   {bb[4] , en},
+//   {d[5]  , en},
+//   {bb[4] , en},//11th bar end
+//   {d[5]  , sn},//12th bar start
+//   {d[5]  , sn},
+//   {f[5]  , en},
+//   {eb[5] , en},
+//   {d[5]  , sn},
+//   {c[5]  , sn},
+//   {c[5]  , en},
+//   {c[5]  , en},
+//   {c[5]  , en},
+//   {b[5]  , en},//12th bar end
+//   {d[5]  , en},//13th bar start
+//   {bb[4] , en},
+//   {bb[4] , en},
+//   {f[4]  , sn},
+//   {f[4]  , sn},
+//   {bb[4] , en},
+//   {bb[4] , en},
+//   {g[4]  , en},
+//   {f[4]  , en},//13th bar end
+//   {d[5]  , sn},//14th bar start
+//   {f[5]  , en},
+//   {d[5]  , sn},
+//   {g[5]  , en},
+//   {d[5]  , sn},
+//   {f[5]  , sn},
+//   {f[5]  , sn},
+//   {d[5]  , sn},
+//   {g[5]  , en},
+//   {d[5]  , en},
+//   {f[5]  , en},//14th bar end
+//   {eb[5] , en},//15th bar start
+//   {d[5]  , en},
+//   {c[5]  , en},
+//   {bb[4] , en},
+//   {c[5]  , en},
+//   {bb[4] , en},
+//   {r     , en},
+//   {f[4]  , en},//15th bar end
+//   {bb[4] , sn},//16th bar start
+//   {bb[4] , sn},
+//   {bb[4] , en},
+//   {bb[4] , sn},
+//   {bb[4] , sn},
+//   {bb[4] , en},
+//   {bb[4] , qn},
+//   {r     , en},
+//   {f[4]  , en},//16th bar end
+//   {d[5]  , sn},//17th bar start
+//   {bb[4] , sn},
+//   {bb[4] , en},
+//   {bb[4] , sn},
+//   {bb[4] , sn},
+//   {g[4]  , en},
+//   {g[4]  , qn},
+//   {r     , qn},//17th bar end
+//   {d[5]  , en},//18th bar start
+//   {bb[4] , qn},
+//   {bb[4] , sn},
+//   {g[4]  , sn},
+//   {bb[4] , en},
+//   {bb[4] , qn},
+//   {bb[4] , sn},
+//   {g[4]  , sn},//18th bar end
+//   {bb[4] , en},//19th bar start
+//   {bb[4] , qn},
+//   {bb[4] , qn},
+//   {d[5]  , qn},
+//   {d[5]  , en},//19th bar end
+//   {d[5]  , en},//20th bar start
+//   {bb[4] , qn},
+//   {bb[4] , sn},
+//   {g[4]  , sn},
+//   {bb[4] , en},
+//   {bb[4] , qn},
+//   {bb[4] , sn},
+//   {g[4]  , sn},//20th bar end
+//   {bb[4] , en},//21st bar start
+//   {bb[4] , qn},
+//   {bb[4] , qn},
+//   {d[5]  , qn},
+//   {d[5]  , en},//21st bar end
+//   {d[5]  , qn},//22nd bar start
+//   {f[5]  , qn},
+//   {eb[5] , en},
+//   {d[5]  , en},
+//   {c[5]  , en},
+//   {c[5]  , en},//22nd bar end
+//   {c[5]  , en},//23rd bar start
+//   {bb[4] , en+qn},
+//   {bb[4] , en},
+//   {bb[4] , en},
+//   {c[5]  , en},
+//   {bb[4] , en},//23rd bar end
+//   {d[5]  , en},//24th bar start
+//   {c[5]  , en+qn},
+//   {c[5]  , en},
+//   {bb[4] , qn},
+//   {c[5]  , en},
+//   {c[5]  , en},
+//   {d[5]  , en},
+//   {g[4]  , hn},
+//   {r     , qn}
+// };
+
+// Darude Sandstorm
+long bpm = 140;
+long darude_start[][2] = { //Loop this twice
+	{b[3]   , qn + en},//1st bar start
+	{b[3]   , qn + en},
+	{b[3]   , en},
+	{b[3]   , qn + en},
+	{b[3]   , qn + en},
+	{e[4]   , en},	   //1st bar end
+	{e[4]   , qn + en},//2nd bar start
+	{e[4]   , qn + en},
+	{d[4]   , en},
+	{d[4]   , qn + en},
+	{d[4]   , qn + en},
+	{a[3]   , en},	   //2nd bar end
+	{b[3]   , qn + en},//3rd bar start
+	{b[3]   , qn + en},
+	{b[3]   , en},
+	{b[3]   , qn + en},
+	{b[3]   , qn + en},
+	{e[4]   , en},	   //3rd bar end
+	{b[3]   , qn + en},//4th bar start
+	{b[3]   , qn + en},
+	{b[3]   , en},
+	{b[3]   , qn + en},
+	{b[3]   , qn + en},
+	{e[4]   , en}	   //4th bar end
 };
 
+long duh_duh_duh_duh[][2] = {
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn}
+};
+
+long duh_duh_duh_duh_da[][2] = duh_duh_duh_duh + {b[2]   , en};
+
+
+long darude_up[][2] = {
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , en},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , en},
+	{e[3]   , sn},
+	{e[3]   , sn}
+};
+
+long darude_up_duh_da[][2] = {
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , en},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , en},
+	{d[3]   , en}
+};
+
+long darude_up_da_da[][2] = {
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , en},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{d[3]   , en},
+	{d[3]   , en}
+};
+
+long darude_down[][2] = {
+	{e[3]   , sn},
+	{e[3]   , sn},
+	{e[3]   , sn},
+	{e[3]   , sn},
+	{e[3]   , en},
+	{d[3]   , sn},
+	{d[3]   , sn},
+	{d[3]   , sn},
+	{d[3]   , sn},
+	{d[3]   , sn},
+	{d[3]   , sn},
+	{d[3]   , en},
+	{a[2]   , sn},
+	{a[2]   , sn}
+};
+
+long duh_duh_duh_da_da[][2] = {
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{b[2]   , en},
+	{d[3]   , en},
+	{d[3]   , en}
+};
+
+long duh_duh_da[][2] = {
+	{b[2]   , sn},
+	{b[2]   , sn},
+	{d[3]   , en}
+};
+//Fix to make it concatenate
+long song[][2] = darude_start + darude_start + duh_duh_duh_duh_da + {r , fn + en + qn} + {d[3] , qn} + duh_duh_duh_duh_da + {r , fn + en + qn + fn} + 4 * duh_duh_duh_duh_da + 7 * duh_duh_duh_duh + {b[3] , qn} + darude_up + darude_down + 3 * darude_up + darude_down + 3 * darude_up + darude_down + 3 * darude_up + darude_down + 2 * darude_up + darude_up_duh_da + darude_up_da_da + darude_up + darude_down + darude_up_duh_da + darude_up_da_da + darude_up + darude_down + 4 * darude_up_da_da + 4* duh_duh_duh_da_da + 8 * duh_duh_da + {e[4], fn};
 
 
 // Whether the speaker is currently playing
