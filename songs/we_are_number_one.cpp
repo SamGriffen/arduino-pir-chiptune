c304f2e10a15ec410492053e7c55294519b27242
Sam Griffen <samgriffen99@gmail.com>
2021-08-28 14:00:43 +1200

    Create we_are_number_one.cpp

---
+++ songs/we_are_number_one.cpp
@@ -0,0 +1,29 @@
+/* WE ARE NUMBER ONE */
+/*
+long bpm = 600;
+long song[][2] = {
+  {f[5],fn + hn},
+  {c[6],hn},
+  {b[5],qn},
+  {c[6],qn},
+  {b[5],qn},
+  {c[6],qn},
+  {b[5],hn},
+  {c[6],hn},
+  {gs[5],fn},
+  {f[5],fn + hn},
+  {f[5],hn},
+  {gs[5],hn},
+  {c[6],hn},
+  {cs[6],fn},
+  {gs[5],fn},
+  {cs[6],fn},
+  {eb[6],fn},
+  {c[6],hn},
+  {cs[6],hn},
+  {c[6],hn},
+  {cs[6],hn},
+  {c[6],fn}
+};
+*/
+/* END WE ARE NUMBER ONE */
